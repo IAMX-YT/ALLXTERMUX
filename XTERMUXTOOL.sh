e8b4532ca0f6a76e912863b48b0a800f94f40924
IAMX <63820814+IAMX-YT@users.noreply.github.com>
2020-09-26 00:48:24 +0530

    Add files via upload

---
+++ XTERMUXTOOL.sh
@@ -0,0 +1,97 @@
+echo "W E L C O M E  TO THE TERMUX TOOL BY IAMX"
+sleep 3
+echo "LET GET START INSTALL PACKAGES"
+sleep 1
+echo " WAIT "
+sleep 1
+echo " FIRST SUBSCRIBE OUR YOUTUBE CHANNEL "
+am start -a android.intent.action.VIEW -d http://www.youtube.com/c/IAMXYT
+#command;>;>>
+apt-get update -y                    
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+pkg install pv
+gem install lolcat
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+toilet " 30℅ PACAKGE INSTALL DONE" -f term -F border --gay | pv -qL 80
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+apt install wcalc -y 
+apt install openssl -y
+apt install bmon -y
+apt-get upgrade -y
+toilet " 50℅ PACAKGE INSTALL DONE" -f term -F border --gay | pv -qL 80
+apt update
+apt upgrade
+apt install figlet
+pkg i git 
+apt install ruby
+apt install neofetch -y
+apt install toilet -y
+apt install wget -y
+apt install tsu -y
+apt install unzip -y
+pkg i dialog
+apt update && apt upgrade
+pkg i python2
+apt upgrade python
+pkg i toilet
+pkg i termux-api
+pkg i toilet
+pkg i wget
+pkg i neofetch
+pkg i tsu
+pkg i sl
+apt install figlet
+apt install cmatrix
+apt install w3m
+pkg i ncurses-utils
+pkg install lolcat
+pkg install openssh
+pkg install php
+pkg install fish -y
+pkg install libcaca -y
+pkg install cowsay -y
+pkg i fortune
+telnet mapscii.me
+toilet " 90℅ PACAKGE INSTALL DONE" -f term -F border --gay | pv -qL 80
+npm install heroku -g
+toilet "X TERMUX TOOL has been install successfully" -f term -F border --gay | pv -qL 80
+sleep 2
+toilet "FOR MORE THIS TYPE OF TOOLS" -f term -F border --gay | pv -qL 80
+sleep 0.8
+toilet "SUBSCRIBE MY YOUTUBE CHANNEL" -f term -F border --gay | pv -qL 80
+sleep 0.8
+toilet "FOR THIS FILE MAKING EFFORTS" -f term -F border --gay | pv -qL 80
+sleep 1
+toilet "Y O U T U B E   O P E N" -f term -F border --gay | pv -qL 80
+am start -a android.intent.action.VIEW -d http://www.youtube.com/c/IAMXYT
+sleep 3
+toilet " 100℅ PACAKGE INSTALL DONE" -f term -F border --gay | pv -qL 80
+#FILE FINISHED
+#COPY KARNE WALE KI MA CHUDE GI
+#JO COPY KAREGA USKI MA GP ROAD PE CHUDEGI
+#COPYPASTER GANDU HAI
+#COPY KARNE WALE KI BHEN KI CHUT ME LAND FAAT JAE
